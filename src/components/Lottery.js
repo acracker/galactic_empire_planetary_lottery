9c16241648a1374f20793471215669bccb9f6fc0
Glauber Mota <glaubernm@gmail.com>
2018-11-13 17:29:58 -0200

    Update: any card is choosable

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Api from '../services/Api';
 import { getRandomIntInclusive } from '../services/Utils';
 import Card from './Card';
@@ -7,6 +8,7 @@ class Lottery extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      chosenCard: null,
       chosenPlanet: null,
       spin: true
     };
@@ -14,13 +16,31 @@ class Lottery extends Component {
     this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
-  handleCardClick() {
+  componentDidUpdate() {
+    const chosenCard = this.state.chosenCard;
+    const chosenPlanet = this.state.chosenPlanet;
+
+    if (chosenCard && chosenPlanet) {
+      for (let i = 0; i < chosenCard.children.length; i++) {
+        if (chosenCard.children[i].className === 'lottery__card-front') {
+          ReactDOM.render(
+            <Card chosenCard={chosenPlanet} />,
+            chosenCard.children[i]
+          );
+        }
+      }
+    }
+  }
+
+  handleCardClick(e) {
     if (this.state.spin === true) {
       this.setState({ spin: false });
+      const el = e.currentTarget;
       const lotteryCards = document.getElementsByClassName('lottery__card');
 
       for (let i = 0; i < lotteryCards.length; i++) {
-        if(i === 0) {
+        if(lotteryCards[i] === el) {
+          this.setState({ chosenCard: lotteryCards[i] });
           lotteryCards[i].classList.add('lottery__card--selected');
         } else {
           lotteryCards[i].classList.add('lottery__card--stop');
@@ -39,44 +59,41 @@ class Lottery extends Component {
   handleButtonClick() {
     if (this.state.spin === false) {
       this.setState({
+        chosenCard: null,
         chosenPlanet: null,
         spin: true
       });
       const lotteryCards = document.getElementsByClassName('lottery__card');
 
       for (let i = 0; i < lotteryCards.length; i++) {
-        if(i === 0) {
-          lotteryCards[i].classList.remove('lottery__card--selected');
-        } else {
-          lotteryCards[i].classList.remove('lottery__card--stop');
-        }
+        lotteryCards[i].classList.remove('lottery__card--selected');
+        lotteryCards[i].classList.remove('lottery__card--stop');
       }
     }
   }
 
   render() {
-    const chosenPlanet = this.state.chosenPlanet;
+    const cards = function() {
+      let cardsArray = [];
+
+      for (let i = 0; i < 3; i++) {
+        cardsArray[i] = (
+          <div key={i} className="lottery__card lottery__card--spin"
+            onClick={(e) => this.handleCardClick(e)}>
+            <div className="lottery__card-back"></div>
+            <div className="lottery__card-front"></div>
+          </div>
+        );
+      }
+
+      return cardsArray;
+    }.bind(this);
 
     return (
       <div className="lottery">
         <div className="lottery__wrapper">
           <div className="lottery__container">
-            <div className="lottery__card lottery__card--spin"
-              onClick={() => this.handleCardClick()}
-            >
-              <div className="lottery__card-back"></div>
-              <div className="lottery__card-front">
-                { chosenPlanet &&
-                  <Card chosenCard={chosenPlanet} />
-                }
-              </div>
-            </div>
-            <div className="lottery__card lottery__card--spin">
-              <div className="lottery__card-back"></div>
-            </div>
-            <div className="lottery__card lottery__card--spin">
-              <div className="lottery__card-back"></div>
-            </div>
+            { cards() }
           </div>
         </div>
         { this.state.spin === false &&
